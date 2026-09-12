80e2eb81d83789e6a7d1b511435cd9810ac935c4
Юки <nenboard65@gmail.com>
2026-09-12 18:04:40 +0300

    Add files via upload

--- kernel.asm
+++ kernel.asm
@@ -55,50 +55,50 @@ back:
 check:
     mov al, 0
     stosb
-
+    
     mov si, enter
     call print
     mov bl, 0
-
+    
     mov si, command
     mov di, command_cls
     mov cx, 4
     repe cmpsb
     je cls
-
+    
     mov si, command
     mov di, command_help
     mov cx, 5
     repe cmpsb
     je print_help
-
+    
     mov si, command
     mov di, command_read
     mov cx, 5
     repe cmpsb
     je read
-
+    
     mov si, command
     mov di, command_reboot
     mov cx, 7
     repe cmpsb
     je reboot
-
+    
     mov si, command
     mov di, command_time
     mov cx, 5
     repe cmpsb
     je time
-
+    
     mov si, command
     mov di, command_write
     mov cx, 6
     repe cmpsb
     je ready
-
+    
     cmp dl, 0
     je return
-
+    
     mov si, error
     call print
     mov si, console
@@ -114,13 +114,13 @@ cls:
     mov ah, 0x00
     mov al, 0x03
     int 0x10
-
+    
     jmp return
 
 print_help:
     mov si, help
     call print
-
+    
     jmp return
 
 reboot:
@@ -149,29 +149,29 @@ time:
     or al, 0x20
     out dx, al
     dec dx
-
+    
     mov al, 13
     mov ah, 0x0e
     int 0x10
     
     mov ah, 0x02
     int 0x1a
-
+    
     mov dl, ch
     call convert
     mov al, ":"
     mov ah, 0x0e
     int 0x10
-
+    
     mov dl, cl
     call convert
     mov al, ":"
     mov ah, 0x0e
     int 0x10
-
+    
     mov dl, dh
     call convert
-
+    
     mov ah, 0x01
     int 0x16
     jz time
@@ -189,22 +189,22 @@ convert:
     shr al, 4
     add al, "0"
     mov bl, al
-
+    
     mov al, bh
     mov ah, 0x0e
     int 0x10
-
+    
     mov al, bl
     mov ah, 0x0e
     int 0x10
-
+    
     mov al, 0
     ret
 
 return:
     mov si, console
     call print
-
+    
     mov si, 0
     mov bx, ds
     mov es, bx
@@ -226,28 +226,19 @@ read:
     mov dh, 0
     mov bx, 0x9e00
     int 0x13
-    jc disk_error
-
+    
     mov si, readed
     call print
-
+    
     mov si, 0x9e00
     call print
-
+    
     mov si, enter
     call print
-
+    
     jmp return
 
 ready:
-    xor ax, ax
-    mov es, ax
-    mov bx, ax
-    mov dl, 0x80
-    mov ah, 0x10
-    int 0x13
-    jc disk_error
-
     mov si, esc
     call print
 
@@ -273,14 +264,14 @@ back_write:
 next:
     mov si, enter
     call print
-
+    
     mov al, 10
     stosb
     mov al, 13
     stosb
-
+    
     mov dl, 0
-
+    
     jmp write
 
 write:
@@ -315,18 +306,13 @@ save:
     mov dl, 0x80
     mov dh, 0
     int 0x13
-
+    
     mov si, enter
     call print
-
+    
     mov si, saved
     call print
-
-    jmp return
-
-disk_error:
-    mov si, disk
-    call print
+    
     jmp return
 
 welcome: db "Welcome to NenOS!", 10, 13, "Type <help> to show available commands.", 10, 13, 0
@@ -334,7 +320,6 @@ console: db "NenOS> ", 0
 esc: db "Press <ESC> to save.", 10, 13, 0
 saved: db "The document was saved.", 10, 13, 0
 readed: db "Document:", 10, 13, 0
-disk: db "Disk error.", 10, 13, 0
 error: db "Unknown command.", 10, 13, 0
 help: db "Available Commands:", 10, 13, "  1. CLS - clear the screen.", 10, 13, "  2. HELP - displaying available commands.", 10, 13, "  3. READ - read the document.", 10, 13, "  4. REBOOT - reboot the computer.", 10, 13, "  5. TIME - launches the watch app.", 10, 13, "  6. WRITE - write the document.", 10, 13, 0
 backspace: db 8, " ", 8, 0
